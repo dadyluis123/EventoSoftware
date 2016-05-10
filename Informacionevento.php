c0173f19d8b82f4cab4bfa09a5aaf5093a09527d
dadyluis123 <kfordonezj@gmail.com>
2016-05-10 16:01:35 -0500

    Create Informacionevento.php

---
+++ Informacionevento.php
@@ -0,0 +1,56 @@
+<html>
+<head>
+</head>
+<title>Informacion de Talleres</title>
+<link rel="shortcut icon" href="iconosistema.jpg">
+<body bgcolor="33CC66">
+ <center><img src="sistema5.png"></font> </center>
+ 
+ 
+ 
+
+</table></center>
+  <center> <table border="0">
+  <tr>
+  <td><h1><font color="33CC66">-------------------------------------------------------------------------------------------------------</font><td><a href="Bienvenidos.php"><img src="cerrar2.png"></a></td>
+  </tr>
+  </table></center>
+   <center><img src="informa1.png"> </center>
+<center> <h1><font color="FFFFFF">Informacion de evento y contenidos a tratarse en los talleres:</font></h1></center>
+<table border="1" bgcolor="FFFFFF">
+<tr>
+<td><center><a href="InicioSesion.php"><img src="tic.jpg"></a></center> </td><td>Taller de las TICS: Las Tecnologías de la Información y la Comunicación (TIC) son todos aquellos recursos, herramientas y programas que se utilizan para procesar, administrar y compartir la información mediante diversos soportes tecnológicos, tales como: computadoras, teléfonos móviles, televisores, reproductores portátiles de audio y video o consolas de juego.
+
+Actualmente el papel de las TIC en la sociedad es muy importante porque ofrecen muchos servicios como: correo electrónico, búsqueda de información, banca online, descarga de música y cine, comercio electrónico, etc. Por esta razón las TIC han incursionado fácilmente en diversos ámbitos de la vida, entre ellos, el de la educación</td>
+</tr>
+
+<tr>
+<td><center><a href="InicioSesion.php"><img src="software.jpg"></a></center> </td><td>Taller de  Software:El software es desarrollado mediante distintos lenguajes de programación, que permiten controlar el comportamiento de una máquina. Estos lenguajes consisten en un conjunto de símbolos y reglas sintácticas y semánticas, que definen el significado de sus elementos y expresiones. Un lenguaje de programación permite a los programadores del software especificar, en forma precisa, sobre qué datos debe operar una computadora.
+
+
+Lee todo en: Definición de software - Qué es, Significado y Concepto 
+</td>
+</tr>
+
+<tr>
+<td><center><a href="InicioSesion.php"><img src="produccion3.jpg"></a></center> </td><td>Taller de  desarrollo de software: como iniciar con la programacion de la aplicacion, asi como tamabien las fases de la aplicacion </td>
+</tr>
+
+<tr>
+<td><center><a href="InicioSesion.php"><img src="hardware4.jpg"></a></center> </td><td>Taller de  Hardware: El hardware suele distinguirse entre básico (los dispositivos necesarios para iniciar el funcionamiento de un ordenador) y complementario (realizan ciertas funciones específicas).
+
+En cuanto a los tipos de hardware, pueden mencionarse a los periféricos de entrada (permiten ingresar información al sistema, como el teclado y el mouse), los periféricos de salida (muestran al usuario el resultado de distintas operaciones realizadas en la computadora. Ejemplo: monitor, impresora), los periféricos de entrada/salida (módems, tarjetas de red, memorias USB), la unidad central de procesamiento o CPU (los componentes que interpretan las instrucciones y procesan los datos) y la memoria de acceso aleatorio o RAM (que se utiliza para el almacenamiento temporal de información).
+
+Lee todo en: Definición de hardware - Qué es, Significado y Concepto 
+</td>
+</tr>
+
+<tr>
+<td><center><a href="InicioSesion.php"><img src="disenio1.jpg"></a></center> </td><td>Taller de  Diseño grafico: El taller se iniciará con introduccion al diseño, combinacion de colores, diseño de garficos, practica en el computador </td>
+</tr>
+</table>
+
+</body>
+
+
+</html>
